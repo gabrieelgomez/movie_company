0ad7e769563fae8303dbfc438044f5439e7ef36f
gabrieelgomez <gagg1707@gmail.com>
2020-04-28 09:25:29 -0400

    remove from routes pie

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  apipie
   namespace :v1 do
     mount_devise_token_auth_for 'User', at: 'auth', defaults: { format: :json }
   end
